2e618914c47b34125a9aed0bdaefa37388f12bbb
Alex Musayev <alex.musayev@gmail.com>
2015-10-17 21:41:52 +0300

    Create and edit account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -7,10 +7,54 @@ class AccountsController < ApplicationController
     @account = Account.new
   end
 
+  def create
+    @account = Account.new(account_params)
+    begin
+      @account.save!
+      redirect_to accounts_path
+    rescue => e
+      flash.now[:alert] = e.message
+      render :new
+    end
+  end
+
   def edit
+    @account = Account.find(id)
+  end
+
+  def update
+    @account = Account.find(id)
+    @account.assign_attributes(account_params)
+    begin
+      @account.save!
+      redirect_to accounts_path, flash: { notify: 'Account updated' }
+    rescue => e
+      flash.now[:alert] = e.message
+      render :edit
+    end
   end
 
   private
+
+  def permitted_params
+    [:title, :description, :id, :currency]
+  end
+
+  def account_params
+    params.require(:account).permit(permitted_params)
+  end
+
+  def id
+    params.require(:id)
+  end
+
+  def title
+    account_params[:title]
+  end
+
+  def description
+    account_params[:description]
+  end
 
   def account_id
     params.require(:id)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def flash_type_to_bs_class(flas_message_type)
+    case flas_message_type.to_s
+    when 'alert'
+      'alert-danger'
+    when 'notice'
+      'alert-success'
+    when 'error'
+      'alert-danger'
+    else
+      'alert-info'
+    end
+  end
 end
