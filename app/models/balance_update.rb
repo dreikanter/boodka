ede76cc9d0d88109fc8e8b1a447aa899576b435d
Alex Musayev <alex.musayev@gmail.com>
2015-10-17 21:41:12 +0300

    Redundant moewl dropped

--- app/models/balance_update.rb
+++
@@ -1,7 +0,0 @@
-class BalanceUpdate < ActiveRecord::Base
-  delegate :currency, to: :account
-
-  monetize :amount_cents, with_model_currency: :currency
-
-  belongs_to :account
-end
--- test/factories/balance_updates.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :balance_update do
-    account_id 1
-amount_cents 1
-description "MyString"
-  end
-
-end
