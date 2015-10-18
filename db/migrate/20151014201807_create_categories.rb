4107d776162e879fce614e57588b5bdc168e9cad
Alex Musayev <alex.musayev@gmail.com>
2015-10-18 20:55:53 +0300

    Category table updated

--- db/migrate/20151014201807_create_categories.rb
+++ db/migrate/20151014201807_create_categories.rb
@@ -1,7 +1,9 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      t.string :title
+      t.string :title, null: false
+      t.string :description, null: false, default: ''
+      t.string :code, null: false, default: ''
 
       t.timestamps null: false
     end
