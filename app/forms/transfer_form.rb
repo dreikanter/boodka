a4ad2cdb964efdfdf5afc09f46b2b18c10ca6faf
Alex Musayev <alex.musayev@gmail.com>
2015-10-19 17:54:09 +0300

    Field renamed

--- app/forms/transfer_form.rb
+++ app/forms/transfer_form.rb
@@ -1,7 +1,5 @@
 class TransferForm < FormObject
-  include Currency
-
-  attr_accessor :currency,
+  attr_accessor :amount_currency,
                 :amount,
                 :from_account_id,
                 :to_account_id,
@@ -9,9 +7,16 @@ class TransferForm < FormObject
 
   validates :from_account_id, :to_account_id, presence: true
   validates :amount, numericality: true
+  validates :amount_currency, inclusion: { in: Const::CURRENCY_CODES }
 
   def self.permitted_params
-    [:from_account_id, :to_account_id, :description, :amount, :currency]
+    [
+      :from_account_id,
+      :to_account_id,
+      :description,
+      :amount,
+      :amount_currency
+    ]
   end
 
   def self.from_params(params)
@@ -26,16 +31,16 @@ class TransferForm < FormObject
 
   def from_transaction_params
     {
-      currency: currency,
-      amount: Float(amount) * -1,
+      amount: -Float(amount),
+      amount_currency: amount_currency,
       account_id: from_account_id
     }
   end
 
   def to_transaction_params
     {
-      currency: currency,
       amount: Float(amount),
+      amount_currency: amount_currency,
       account_id: to_account_id
     }
   end
