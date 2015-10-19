85f9a8daf04fe04604f0f21a289bc852a296f40c
Alex Musayev <alex.musayev@gmail.com>
2015-10-19 17:14:34 +0300

    Redundant column

--- db/migrate/20151014201511_create_transactions.rb
+++ db/migrate/20151014201511_create_transactions.rb
@@ -2,9 +2,8 @@ class CreateTransactions < ActiveRecord::Migration
   def change
     create_table :transactions do |t|
       t.integer :account_id, null: false
-      t.decimal :amount_cents, null: false, default: 0, precision: 8, scale: 0
-      t.string :currency, null: false
-      t.decimal :calculated_amount_cents, null: false, default: 0, precision: 8, scale: 0
+      t.monetize :amount, null: false, default: 0
+      t.monetize :calculated_amount, null: false, default: 0
       t.float :rate, null: false, default: 1.0
       t.integer :category_id
       t.integer :transfer_id
