c5c4c08eeb899bd92449f635893958a08a632d27
Alex Musayev <alex.musayev@gmail.com>
2015-10-19 19:58:30 +0300

    Default

--- db/migrate/20151017224925_create_reconciliations.rb
+++ db/migrate/20151017224925_create_reconciliations.rb
@@ -2,7 +2,7 @@ class CreateReconciliations < ActiveRecord::Migration
   def change
     create_table :reconciliations do |t|
       t.integer :account_id, null: false
-      t.decimal :amount_cents, null: false, precision: 8, scale: 0
+      t.decimal :amount_cents, null: false, precision: 8, scale: 0, default: 0
 
       t.timestamps null: false
     end
