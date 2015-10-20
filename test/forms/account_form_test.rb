77955c20e68d65c2a29c3f3493be1580d27cca7c
Alex Musayev <alex.musayev@gmail.com>
2015-10-20 23:13:59 +0300

    Obsolete code

--- test/forms/account_form_test.rb
+++ test/forms/account_form_test.rb
@@ -1,27 +1,27 @@
-require 'test_helper'
+# require 'test_helper'
 
-describe AccountForm do
-  let(:account_form_params) { build :account_form_params }
-  let(:account_form) { build :account_form }
-  let(:default_account_form) { build :default_account_form }
+# describe AccountForm do
+#   let(:account_form_params) { build :account_form_params }
+#   let(:account_form) { build :account_form }
+#   let(:default_account_form) { build :default_account_form }
 
-  it 'process account params' do
-    account_form.account_params.must_equal({
-      title: account_form_params[:title],
-      description: account_form_params[:description],
-      currency: account_form_params[:currency],
-      default: account_form_params[:default]
-    })
-  end
+#   it 'process account params' do
+#     account_form.account_params.must_equal({
+#       title: account_form_params[:title],
+#       description: account_form_params[:description],
+#       currency: account_form_params[:currency],
+#       default: account_form_params[:default]
+#     })
+#   end
 
-  it 'process reconciliation params' do
-    account_form.reconciliation_params.must_equal({
-      amount: account_form_params[:amount],
-      created_at: account_form_params[:created_at]
-    })
-  end
+#   it 'process reconciliation params' do
+#     account_form.reconciliation_params.must_equal({
+#       amount: account_form_params[:amount],
+#       created_at: account_form_params[:created_at]
+#     })
+#   end
 
-  it 'precoss default flag' do
-    default_account_form.default?.must_equal true
-  end
-end
+#   it 'precoss default flag' do
+#     default_account_form.default?.must_equal true
+#   end
+# end
