c2705eba25ade7c9885901e9296dc5f0e287d5cc
Alex Musayev <alex.musayev@gmail.com>
2015-10-24 18:33:14 +0300

    Account title format

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -4,4 +4,8 @@ class AccountDecorator < Draper::Decorator
   def display_title
     "#{model.title}#{' (default)' if model.default}"
   end
+
+  def display_title_with_currency
+    "#{model.title} (#{model.currency})#{' (default)' if model.default}"
+  end
 end
