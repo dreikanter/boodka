31ba91fe7a68d9729cd8fa0f2f1f455dc019b7bb
Alex Musayev <alex.musayev@gmail.com>
2015-10-28 23:01:16 +0300

    Cleanup

--- app/calculators/totals_per_currency_calculator.rb
+++ app/calculators/totals_per_currency_calculator.rb
@@ -14,8 +14,4 @@ class TotalsPerCurrencyCalculator < BasicCalculator
     total = -> (a) { Calc.account_total(account: a) }
     @accounts.map { |a| { currency: a.currency, amount: total.call(a) } }
   end
-
-  # def converted_equivalent(totals_per_currency)
-  #   Hash[totals_per_currency.map { |currency, amount| [currency, totals_per_currency.values.sum.exchange_to(currency)] }]
-  # end
 end
