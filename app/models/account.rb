3269b08857cb89d6f98a90fc53c38087e56386b9
Alex Musayev <alex.musayev@gmail.com>
2015-10-29 00:34:23 +0300

    Unused associations

--- app/models/account.rb
+++ app/models/account.rb
@@ -17,8 +17,6 @@ class Account < ActiveRecord::Base
   validates :currency, inclusion: { in: Const::CURRENCY_CODES }
 
   has_many :transactions, dependent: :destroy
-  has_many :planned_transactions, dependent: :destroy
-  has_many :reconciliations, dependent: :destroy
 
   scope :ordered, -> { order(:created_at) }
 
