d780deeffa6cf7c2c47a61e2309dc2487aed9416
Alex Musayev <alex.musayev@gmail.com>
2015-10-30 21:43:03 +0300

    Accounts difference validation

--- app/forms/transfer_form.rb
+++ app/forms/transfer_form.rb
@@ -9,4 +9,13 @@ class TransferForm < Reform::Form
 
   validates :amount, :currency, :from_account_id, :to_account_id, presence: true
   validates :amount, numericality: true
+  validate :accounts_are_different
+
+  private
+
+  def accounts_are_different
+    if from_account_id == to_account_id
+      errors[:to_account_id] << 'destination account should be different'
+    end
+  end
 end
