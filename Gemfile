b9ea7bf96ef3316e27d1f9d4908a441dfcb42145
Alex Musayev <alex.musayev@gmail.com>
2015-10-31 18:34:44 +0300

    SQLite -> Postgres

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jquery-turbolinks'
 gem 'js-routes'
 gem 'momentjs-rails', '>= 2.9.0'
 gem 'bootstrap3-datetimepicker-rails', '~> 4.14.30'
+gem 'pg'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-font-awesome'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151024224925) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string   "currency",                   null: false
     t.string   "title",                      null: false
