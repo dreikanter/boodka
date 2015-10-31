2a06963e88e60616f732193e6a188db5c2cdbb20
Alex Musayev <alex.musayev@gmail.com>
2015-10-31 13:56:38 +0300

    Buttons builder

--- app/service/smart_builder.rb
+++ app/service/smart_builder.rb
@@ -1,7 +1,7 @@
 class SmartBuilder < ActionView::Helpers::FormBuilder
-  def group(field)
+  def group(field = nil)
     classes = ['form-group']
-    classes << 'has-error' if errors(field).any?
+    classes << 'has-error' if (field && errors(field).any?)
     @template.content_tag(:div, class: classes.join(' ')) { yield }
   end
 
@@ -27,6 +27,14 @@ class SmartBuilder < ActionView::Helpers::FormBuilder
     end
   end
 
+  def submit_group(options = {})
+    group do
+      submit(options[:caption], class: 'btn btn-default',
+        data: { disable_with: 'Processing...' }) + ' ' +
+        (options[:cancel] ? cancel_button : '')
+    end
+  end
+
   private
 
   def errors(field)
@@ -35,5 +43,9 @@ class SmartBuilder < ActionView::Helpers::FormBuilder
 
   def field_errors(field)
     @template.content_tag(:span, errors(field).join(', '), class: 'help-block')
+  end
+
+  def cancel_button
+    @template.link_to('Cancel', :back, class: 'btn btn-default')
   end
 end
