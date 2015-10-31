93bb8c1cd47e5206c69524bd369de5f48e13a2f2
Alex Musayev <alex.musayev@gmail.com>
2015-10-31 17:05:34 +0300

    Transactions index layout

--- app/decorators/transaction_decorator.rb
+++ app/decorators/transaction_decorator.rb
@@ -1,8 +1,19 @@
 class TransactionDecorator < Draper::Decorator
   delegate_all
 
+  # TODO: Remove extra query
   def display_category
     return model.category.try(:title) if model.outflow?
-    h.content_tag :span, 'Income', class: 'positive'
+    classes = "kind kind-#{model.kind}"
+    h.content_tag :span, model.kind.to_s.capitalize, class: classes
+  end
+
+  def display_amount
+    model.amount.format(symbol: false, no_cents: false)
+  end
+
+  def amount_currency_label
+    classes = "currency currency-#{model.amount_currency}"
+    h.content_tag :span, model.amount_currency, class: classes
   end
 end
