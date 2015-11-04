671fe63c24684967d2873961c688613dd9bae1cb
Alex Musayev <alex.musayev@gmail.com>
2015-11-04 14:40:02 +0300

    Unused helpers dropped

--- app/helpers/budget_helper.rb
+++
@@ -1,5 +0,0 @@
-module BudgetHelper
-  def cell_id(prefix, year, month, category)
-    [prefix, year, month, category].join('-')
-  end
-end
--- app/helpers/period_helper.rb
+++ app/helpers/period_helper.rb
@@ -16,8 +16,4 @@ module PeriodHelper
       period_nav_link(currenct_date.year, month, currenct_date.month)
     end.join.html_safe
   end
-
-  def cell_id(prefix, period, category)
-    [prefix, period.year, period.month, category.id].join('-')
-  end
 end
