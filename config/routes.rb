0a67caed4b3190ca1deab4333f7d36acaafed3b8
Alex Musayev <alex.musayev@gmail.com>
2015-11-12 23:51:08 +0300

    Paths renamed

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :categories, except: :show
   resources :transfers, except: [:show]
 
-  get 'budget' => 'periods#show', as: :current_budget
+  get 'budget' => 'periods#show', as: :current_period
 
   scope 'budget/:year/:month' do
     resource :period, only: :show, shallow: true, path: ''
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
               path: 'categories', constraints: { :format => /(js|json)/ }
   end
 
-  get 'history' => 'operations#index', as: :current_history
+  get 'history' => 'operations#index', as: :current_operations
 
   scope 'history/:year/:month' do
     resources :operations, only: :index, shallow: true, path: ''
