bd2db016c717b9e692fe20d197f9102648fe0d16
Alex Musayev <alex.musayev@gmail.com>
2015-11-12 23:52:07 +0300

    Operation params

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,19 +1,41 @@
 class OperationsController < ApplicationController
   include Periodical
 
+  OPS = %w(transaction reconciliation transfer)
+
   before_action :load_history
 
   private
 
-  OPS = [Transaction, Reconciliation, Transfer]
-
   def load_history
-    historical_frame = -> (model) { model.history.where(created_at: time_frame) }
-    by_creation = -> (a, b) { b.created_at <=> a.created_at }
-    @history = OPS.map(&historical_frame).flatten.sort(&by_creation)
+    ops = operation_types.map { |klass| historical_frame(klass) }.flatten
+    @history = ops.sort { |a, b| b.created_at <=> a.created_at }
+  end
+
+  def operation_types
+    fail 'Illegal operation type' if operation && !OPS.include?(operation)
+    (operation && [operation] || OPS).map { |n| n.classify.constantize }
+  end
+
+  def historical_frame(model)
+    model.history.where(historical_frame_criteria(model))
+  end
+
+  def historical_frame_criteria(model)
+    criteria = { created_at: time_frame }
+    expenses = (model == Transaction && category_id)
+    criteria.merge(expenses ? { category_id: category_id } : {})
   end
 
   def time_frame
     start_date.beginning_of_month..start_date.end_of_month
+  end
+
+  def category_id
+    params[:category_id]
+  end
+
+  def operation
+    params[:operation]
   end
 end
