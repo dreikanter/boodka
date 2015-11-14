7e0f6e53f60e580dda6fe4a1b4ed37031c69cb0d
Alex Musayev <alex.musayev@gmail.com>
2015-11-15 00:20:47 +0300

    Annotation

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -8,8 +8,8 @@
 #  updated_at      :datetime         not null
 #  amount_cents    :integer          default(0), not null
 #  amount_currency :string           default("USD"), not null
-#  from_account_id :integer
-#  to_account_id   :integer
+#  from_account_id :integer          not null
+#  to_account_id   :integer          not null
 #
 
 class Transfer < ActiveRecord::Base
