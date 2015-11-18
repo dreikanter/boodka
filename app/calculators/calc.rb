10c8e57d62c85bb24090637d0f539d385031a567
Alex Musayev <alex.musayev@gmail.com>
2015-11-18 18:21:07 +0300

    Logging

--- app/calculators/calc.rb
+++ app/calculators/calc.rb
@@ -1,5 +1,6 @@
 class Calc
   def self.method_missing(calculator_name, *args)
+    Log.info "Calculating #{calculator_name}"
     "#{calculator_name}_calculator".classify.constantize.new(*args).calculate
   end
 end
