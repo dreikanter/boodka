b95e478cc930e0998688f29d7b33361fce38db8c
Alex Musayev <alex.musayev@gmail.com>
2019-06-04 21:32:55 +0300

    Add sample exchange rates to MoneyRails configuration #115

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,6 +1,111 @@
-require 'money/bank/google_currency'
-
 MoneyRails.configure do |config|
-  config.default_bank = Money::Bank::GoogleCurrency.new
-  Money::Bank::GoogleCurrency.ttl_in_seconds = Const::RATES_TTL_IN_SECONDS
+
+  # To set the default currency
+  #
+  # config.default_currency = :usd
+
+  # Set default bank object
+  #
+  # Example:
+  # config.default_bank = EuCentralBank.new
+
+  # Add exchange rates to current money bank object.
+  # (The conversion rate refers to one direction only)
+  #
+  # Example:
+  # config.add_rate "USD", "CAD", 1.24515
+  # config.add_rate "CAD", "USD", 0.803115
+
+  # Sample data for testing
+
+  config.add_rate "USD", "CAD", 1.24515
+  config.add_rate "CAD", "USD", 0.803115
+
+  config.add_rate "USD", "RUB", 65.02
+  config.add_rate "USD", "EUR", 0.89
+  config.add_rate "USD", "GBP", 0.79
+
+  config.add_rate "RUB", "USD", 0.015
+  config.add_rate "RUB", "EUR", 0.014
+  config.add_rate "RUB", "GBP", 0.012
+
+  config.add_rate "EUR", "RUB", 73.20
+  config.add_rate "EUR", "USD", 1.13
+  config.add_rate "EUR", "GBP", 0.89
+
+  config.add_rate "GBP", "RUB", 82.64
+  config.add_rate "GBP", "USD", 1.27
+  config.add_rate "GBP", "EUR", 1.13
+
+  # To handle the inclusion of validations for monetized fields
+  # The default value is true
+  #
+  # config.include_validations = true
+
+  # Default ActiveRecord migration configuration values for columns:
+  #
+  # config.amount_column = { prefix: '',           # column name prefix
+  #                          postfix: '_cents',    # column name  postfix
+  #                          column_name: nil,     # full column name (overrides prefix, postfix and accessor name)
+  #                          type: :integer,       # column type
+  #                          present: true,        # column will be created
+  #                          null: false,          # other options will be treated as column options
+  #                          default: 0
+  #                        }
+  #
+  # config.currency_column = { prefix: '',
+  #                            postfix: '_currency',
+  #                            column_name: nil,
+  #                            type: :string,
+  #                            present: true,
+  #                            null: false,
+  #                            default: 'USD'
+  #                          }
+
+  # Register a custom currency
+  #
+  # Example:
+  # config.register_currency = {
+  #   priority:            1,
+  #   iso_code:            "EU4",
+  #   name:                "Euro with subunit of 4 digits",
+  #   symbol:              "€",
+  #   symbol_first:        true,
+  #   subunit:             "Subcent",
+  #   subunit_to_unit:     10000,
+  #   thousands_separator: ".",
+  #   decimal_mark:        ","
+  # }
+
+  # Specify a rounding mode
+  # Any one of:
+  #
+  # BigDecimal::ROUND_UP,
+  # BigDecimal::ROUND_DOWN,
+  # BigDecimal::ROUND_HALF_UP,
+  # BigDecimal::ROUND_HALF_DOWN,
+  # BigDecimal::ROUND_HALF_EVEN,
+  # BigDecimal::ROUND_CEILING,
+  # BigDecimal::ROUND_FLOOR
+  #
+  # set to BigDecimal::ROUND_HALF_EVEN by default
+  #
+  # config.rounding_mode = BigDecimal::ROUND_HALF_UP
+
+  # Set default money format globally.
+  # Default value is nil meaning "ignore this option".
+  # Example:
+  #
+  # config.default_format = {
+  #   no_cents_if_whole: nil,
+  #   symbol: nil,
+  #   sign_before_symbol: nil
+  # }
+
+  # Set default raise_error_on_money_parsing option
+  # It will be raise error if assigned different currency
+  # The default value is false
+  #
+  # Example:
+  # config.raise_error_on_money_parsing = false
 end
