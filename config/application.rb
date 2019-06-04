f739ffad27bf95e302c20b3696ca6af271335b63
Alex Musayev <alex.musayev@gmail.com>
2019-06-04 19:47:29 +0300

    Update to Rails 5 (rails app:update) #115

--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,19 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Boodka
   class Application < Rails::Application
-    config.active_record.raise_in_transactional_callbacks = true
-    config.autoload_paths << Rails.root.join('app', 'decorators')
-    config.autoload_paths << Rails.root.join('app', 'calculators')
-    config.autoload_paths << Rails.root.join('app', 'models', 'concerns')
-    config.autoload_paths << Rails.root.join('app', 'presenters')
-    config.autoload_paths << Rails.root.join('app', 'facades')
-    config.time_zone = 'UTC'
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
 
-    config.generators do |g|
-      g.test_framework :minitest, spec: true, fixture: false
-      g.stylesheets = false
-      g.javascripts = false
-    end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
