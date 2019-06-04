295df55c9d42e6474f85a912e24f6f02f049a4cc
Alex Musayev <alex.musayev@gmail.com>
2019-06-04 20:07:08 +0300

    Update obsolete callback definition for Rails 5 #115

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  around_filter :set_time_zone
+  around_action :set_time_zone
 
   def set_time_zone(&block)
     Time.use_zone(Const::TZ, &block)
